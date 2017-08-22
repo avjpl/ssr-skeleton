66cb850c776ba3b866a4045033c49a2bcf419a45
Adrian Lawrence <avjpldeveloper@aol.com>
2017-08-22 06:45:07 +0100

    Removed un-used plugin

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DashboardPlugin = require('webpack-dashboard/plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
